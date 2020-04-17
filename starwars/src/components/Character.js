22293bbfc085b10a875e61ced1bb21502cbd1283
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-17 09:57:23 -0700

    styling added, elements added to each character card

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -7,12 +7,17 @@ import styled from 'styled-components'
 const Character = ({ character }) => {
      
      const Container = styled.div`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
           border: 1px solid rgb(210, 210, 210);
-          box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+          border-radius: 5px;
+          box-shadow: 10px 8px 12px -2px rgb(128, 127, 197);
           margin: 8px;
           padding: 12px;
           background-color: white;
           width: 50%;
+          background-image: url('https://cdn.spacetelescope.org/archives/images/screen/heic1909a.jpg');
 
           a {
           color: green;
@@ -24,12 +29,27 @@ const Character = ({ character }) => {
           }
 
           `
+     const InfoWrapper = styled.div`
+          border: 1.2px solid yellow;
+          box-shadow: 2px 5px 10px -2px rgb(828, 827, 197);
+          margin: 2% 0;
+          width: 50%;
+          color: ${props => props.textColor ? props.textColor : 'inherit'};
+          &:hover {
+               color: red;
+               transition: all 1s ease-in-out;
+          }
+     `
+     
    
      return (
      <Container>
-          <div>
-               Name: {character.name}
-          </div>
+          <InfoWrapper textColor='yellow'>
+               <span>Name: {character.name}</span>
+          </InfoWrapper>
+          <InfoWrapper textColor='yellow'>
+               Birth Year: {character.birth_year}
+          </InfoWrapper>
                {/* <Character /> */}
      </Container>
 
