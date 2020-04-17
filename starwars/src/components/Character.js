70d2b992d35b8fc3604a33fb5f25c71d21c74828
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-17 08:26:41 -0700

    initial commit - CharactersPage, Character, and favicon updated

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+const Character = () => {
+     // Try to think through what state you'll need for this app before starting. Then build out
+     // the state properties here.
+   
+     // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+     // side effect in a component, you want to think about which state and/or props it should
+     // sync up with, if any.
+   
+     return (
+       
+          <div></div>
+
+
+     );
+   }
+   
+   export default Character;
---
+++ starwars/src/components/CharactersPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Character from './Character'
+import styled from 'styled-components'
+
+
+
+const CharactersPage = ({ characters }) => {
+     // set up state for your data
+     return (
+       <div className="posts-container-wrapper">
+         {
+          characters.map(individualCharacter => {
+            return <Character key={''} character={individualCharacter}/>
+         })
+       }
+       </div>
+     );
+   };
+   
+   export default CharactersPage;
